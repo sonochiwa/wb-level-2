480b2cb693b53c0486973e86f6faaecb1420d59b
Рашид Мамаримов <sonochiwaxd@gmail.com>
2023-12-29 08:49:37 +0600

    dev02

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -25,21 +25,28 @@ import (
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-// unpack - распаковывает строку содержащую повторяющиеся символы
+// Метод unpack распаковывает строку, содержащую повторяющиеся символы
 func unpack(s []rune) string {
 	// Планируется частая конкатенация, поэтому используем string builder
 	var result strings.Builder
 
+	// Итерируем по массиву рун, для поддержки unicode символов
 	for i := 0; i < len(s); i++ {
-		if unicode.IsDigit(s[i]) {
-			// Конвертируем в int
-			count, _ := strconv.Atoi(string(s[i]))
-
-			// Распаковываем
-			result.WriteString(strings.Repeat(string(s[i-1]), count-1))
+		// Если s[i] не экранирующий символ
+		if s[i] == '\\' {
+			result.WriteString(string(s[i+1]))
+			i++
 		} else {
-			// Если char не число, тогда пишем его в result
-			result.WriteString(string(s[i]))
+			// Если s[i] число, то
+			if unicode.IsDigit(s[i]) {
+				// Конвертируем в int
+				count, _ := strconv.Atoi(string(s[i]))
+				// Распаковываем и записываем в result
+				result.WriteString(strings.Repeat(string(s[i-1]), count-1))
+			} else {
+				// Если s[i] не число, тогда пишем его в result
+				result.WriteString(string(s[i]))
+			}
 		}
 	}
 
@@ -49,7 +56,7 @@ func unpack(s []rune) string {
 
 func main() {
 	// Входные данные
-	s := []rune("a4bc2d5e")
+	s := []rune("qwe\\4\\5")
 
 	// Распаковываем строку
 	result := unpack(s)
