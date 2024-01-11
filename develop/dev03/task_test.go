743f0c5d8ff2283025a88efc10a4b080be2d8f6b
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-01-12 01:53:00 +0600

    dev03

--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -3,47 +3,86 @@ package main
 import (
 	"errors"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
 // Test sort algorithm
-func TestSortStrings(t *testing.T) {
-
-}
+//func TestSortStrings(t *testing.T) {
+//
+//}
 
 // Test -k flag
-func TestSetColumn(t *testing.T) {
-
-}
+//func TestSetColumn(t *testing.T) {
+//
+//}
 
 // Test -n flag
-func TestSortNumbers(t *testing.T) {
-
-}
+//func TestSortNumbers(t *testing.T) {
+//
+//}
 
 // Test -r flag
-func TestReverseData(t *testing.T) {
-
-}
+//func TestReverseData(t *testing.T) {
+//
+//}
 
 // Test -u flag
 func TestDedupeData(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    [][]string
+		expected [][]string
+	}{
+		{
+			name:     "no duplicates",
+			input:    [][]string{{"data1"}, {"data2"}, {"data3"}},
+			expected: [][]string{{"data1"}, {"data2"}, {"data3"}},
+		},
+		{
+			name:     "with duplicates",
+			input:    [][]string{{"duplicate"}, {"duplicate"}},
+			expected: [][]string{{"duplicate"}},
+		},
+		{
+			name:     "multiple duplicates",
+			input:    [][]string{{"multi"}, {"multi"}, {"multi"}},
+			expected: [][]string{{"multi"}},
+		},
+		//{
+		//	name:     "empty input",
+		//	input:    [][]string{},
+		//	expected: [][]string{},
+		//},
+	}
 
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			input := dedupeData(tc.input)
+
+			if !reflect.DeepEqual(input, tc.expected) {
+				t.Errorf("TestDedupeData(%s) input \"%v\", exptected \"%v\"", tc.name, input, tc.expected)
+			}
+
+		})
+	}
 }
 
 // Test -M flag
 func TestSortMonths(t *testing.T) {
-	tc := struct {
+	test := struct {
+		name     string
 		input    [][]string
 		expected [][]string
 	}{
-		[][]string{
+		name: "test sort months",
+		input: [][]string{
 			{"february"},
 			{"april"},
 			{"january"},
 			{"march"},
 		},
-		[][]string{
+		expected: [][]string{
 			{"january"},
 			{"february"},
 			{"march"},
@@ -51,69 +90,77 @@ func TestSortMonths(t *testing.T) {
 		},
 	}
 
-	result := sortMonths(tc.input)
+	t.Run(test.name, func(t *testing.T) {
+		result := sortMonths(test.input)
 
-	for i := range result {
-		if result[i][0] != tc.expected[i][0] {
-			t.Errorf("Test case %d: expected \"%s\", got \"%s\"", i+1, tc.expected, result)
-			return
-		}
-	}
-}
-
-// Test -b flag
-func TestTrimSpace(t *testing.T) {
-
-}
-
-// Test -c flag
-func TestCompare(t *testing.T) {
-	tc := []struct {
-		input1   [][]string
-		input2   [][]string
-		expected error
-	}{
-		{
-			[][]string{{"a"}, {"b"}, {"c"}, {"d"}},
-			[][]string{{"a"}, {"b"}, {"c"}, {"d"}},
-			nil,
-		},
-		{
-			[][]string{{"a"}, {"b"}, {"c"}, {"d"}},
-			[][]string{{"b"}, {"a"}, {"c"}, {"d"}},
-			errors.New(fmt.Sprint("disorder:a")),
-		},
-	}
-
-	t.Run("TestCompare", func(t *testing.T) {
-		for i := range tc {
-			err := compare(tc[i].input1, tc[i].input2)
-			if err != nil {
-				if err.Error() != tc[i].expected.Error() {
-					t.Errorf("Test case %d: expected \"%s\", got \"%s\"", i+1, tc[i].expected, err)
-					return
-				}
+		for i := range result {
+			if result[i][0] != test.expected[i][0] {
+				t.Errorf("Test case %d: expected \"%s\", got \"%s\"", i+1, test.expected, result)
+				return
 			}
 		}
 	})
 }
 
+// Test -b flag
+//func TestTrimSpace(t *testing.T) {
+//
+//}
+
+// Test -c flag
+func TestCompare(t *testing.T) {
+	tests := []struct {
+		name     string
+		input1   [][]string
+		input2   [][]string
+		expected error
+	}{
+		{
+			name:     "test case without error",
+			input1:   [][]string{{"a"}, {"b"}, {"c"}, {"d"}},
+			input2:   [][]string{{"a"}, {"b"}, {"c"}, {"d"}},
+			expected: nil,
+		},
+		{
+			name:     "test case with error",
+			input1:   [][]string{{"a"}, {"b"}, {"c"}, {"d"}},
+			input2:   [][]string{{"b"}, {"a"}, {"c"}, {"d"}},
+			expected: errors.New(fmt.Sprint("disorder:a")),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+
+			err := compare(tc.input1, tc.input2)
+			if err != nil {
+				if err.Error() != tc.expected.Error() {
+					t.Errorf("Test case %s: expected \"%s\", got \"%s\"", tc.name, tc.expected, err)
+					return
+				}
+			}
+		})
+	}
+}
+
 // Test -H flag
 func TestHumanNumericSort(t *testing.T) {
-	tc := struct {
+	test := struct {
+		name     string
 		input    [][]string
 		expected [][]string
 	}{
-		[][]string{{"1M"}, {"4"}, {"3K"}, {"2K"}},
-		[][]string{{"4"}, {"2K"}, {"3K"}, {"1M"}},
+		name:     "test success case",
+		input:    [][]string{{"1M"}, {"4"}, {"3K"}, {"2K"}},
+		expected: [][]string{{"4"}, {"2K"}, {"3K"}, {"1M"}},
 	}
 
-	t.Run("TestHumanNumericSort", func(t *testing.T) {
-		result := humanNumericSort(tc.input)
+	t.Run(test.name, func(t *testing.T) {
+		result := humanNumericSort(test.input)
 
 		for i := range result {
-			if tc.expected[i][0] != result[i][0] {
-				t.Errorf("Test case %d: expected \"%s\", got %s", i+1, tc.expected, result)
+			if test.expected[i][0] != result[i][0] {
+				t.Errorf("Test case %s: expected \"%s\", got %s", test.name, test.expected, result)
 				return
 			}
 		}
