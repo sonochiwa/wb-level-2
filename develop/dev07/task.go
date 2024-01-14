88b7a24e0474987f1f62b4c22fa0688443b1b0ad
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-01-14 21:02:23 +0600

    dev07

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -33,6 +38,60 @@ start := time.Now()
 fmt.Printf(“fone after %v”, time.Since(start))
 */
 
-func main() {
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	// Создаем канал, который будем возвращать
+	orChannel := make(chan interface{})
 
+	// Запускаем горутину для каждого входящего канала
+	for _, ch := range channels {
+		go func(ch <-chan interface{}) {
+			// Копируем данные из входящего канала в результирующий
+			for val := range ch {
+				orChannel <- val
+			}
+		}(ch)
+	}
+
+	// Запускаем горутину, которая закроет результирующий канал при закрытии любого из входящих каналов
+	go func() {
+		// Создаем канал для сигнала о закрытии
+		done := make(chan struct{})
+		defer close(done)
+
+		// Ожидаем закрытия любого из входящих каналов или завершения работы всех горутин
+		select {
+		case <-done:
+		case <-channels[len(channels)-1]:
+		}
+
+		// Закрываем результирующий канал
+		close(orChannel)
+	}()
+
+	return orChannel
+}
+
+func sig(after time.Duration) <-chan interface{} {
+	c := make(chan interface{})
+
+	go func() {
+		defer close(c)
+		time.Sleep(after)
+	}()
+
+	return c
+}
+
+func main() {
+	start := time.Now()
+
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Second),
+	)
+
+	fmt.Printf("Done after %v\n", time.Since(start))
 }
