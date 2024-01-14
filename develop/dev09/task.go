79491ab3c480f7abb9a57af0c7baeb36790f4218
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-01-14 21:30:47 +0600

    dev09

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+)
+
 /*
 === Утилита wget ===
 
@@ -8,6 +16,54 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func download(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Ошибка при запросе:", err)
+		return
+	}
+	defer resp.Body.Close()
 
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Ошибка при запросе. Код статуса:", resp.StatusCode)
+		return
+	}
+
+	// Извлекаем имя файла из URL
+	fileName := path.Base(url)
+
+	// Создаем директорию для сохранения файлов
+	err = os.MkdirAll(fileName, os.ModePerm)
+	if err != nil {
+		fmt.Println("Ошибка при создании директории:", err)
+		return
+	}
+
+	// Создаем файл для сохранения данных
+	filePath := path.Join(fileName, "index.html")
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Println("Ошибка при создании файла:", err)
+		return
+	}
+	defer file.Close()
+
+	// Копируем данные из ответа в файл
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		fmt.Println("Ошибка при копировании данных:", err)
+		return
+	}
+
+	fmt.Printf("Сайт успешно скачан в директорию: %s\n", fileName)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Использование: go run main.go <URL>")
+		os.Exit(1)
+	}
+
+	url := os.Args[1]
+	download(url)
 }
--- develop/dev09/task_test.go
+++ develop/dev09/task_test.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestDownload(t *testing.T) {
+	// Создаем временный HTTP сервер для тестов
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, Test!"))
+	}))
+	defer ts.Close()
+
+	// Получаем адрес временного сервера
+	url := ts.URL
+
+	// Вызываем функцию download
+	download(url)
+
+	// Проверяем, что директория была создана
+	dirName := filepath.Base(url)
+	_, err := os.Stat(dirName)
+	if os.IsNotExist(err) {
+		t.Errorf("Ожидалось, что директория %s будет создана, но не создана", dirName)
+	}
+
+	// Проверяем, что файл был создан и содержит ожидаемые данные
+	filePath := filepath.Join(dirName, "index.html")
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		t.Errorf("Ошибка при чтении файла %s: %v", filePath, err)
+	}
+
+	expectedContent := "Hello, Test!"
+	if string(fileContent) != expectedContent {
+		t.Errorf("Ожидалось, что содержимое файла %s будет '%s', но получено '%s'", filePath, expectedContent, string(fileContent))
+	}
+}
