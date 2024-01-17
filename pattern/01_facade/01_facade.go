0214d8b99450af3ccc33422aaaa27d3cb0b06a68
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-01-17 21:51:08 +0600

    facade

--- pattern/01_facade/01_facade.go
+++ pattern/01_facade/01_facade.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
 )
 
 /*
 	Реализовать паттерн «фасад».
-Объяснить применимость паттерна, его плюсы и минусы,а также реальные примеры использования данного примера на практике.
+Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Facade_pattern
 */
 
@@ -32,144 +30,55 @@ import (
 через этот объект
 */
 
-type Product struct {
-	Name  string
-	Price float64
+// SubsystemA - подсистема A
+type SubsystemA struct {
 }
 
-type Shop struct {
-	Name     string
-	Products []Product
+func (s *SubsystemA) OperationA() {
+	fmt.Println("Subsystem A: Operation A")
 }
 
-type Bank struct {
-	Name  string
-	Cards []Card
+// SubsystemB - подсистема B
+type SubsystemB struct {
 }
 
-type Card struct {
-	Name    string
-	Balance float64
-	Bank    *Bank
+func (s *SubsystemB) OperationB() {
+	fmt.Println("Subsystem B: Operation B")
 }
 
-type User struct {
-	Name string
-	Card *Card
+// SubsystemC - подсистема C
+type SubsystemC struct {
 }
 
-func (user User) GetBalance() float64 {
-	return user.Card.Balance
+func (s *SubsystemC) OperationC() {
+	fmt.Println("Subsystem C: Operation C")
 }
 
-// Sell - функция
-func (shop Shop) Sell(user User, product string) error {
-	println("[Магазин] Запрос к пользователю, для получения отстатка по карте")
-	time.Sleep(500 * time.Millisecond)
-	err := user.Card.CheckBalance()
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("[Магазин] Проверка - может ли [%s], купить товар\n", user.Name)
-	time.Sleep(500 * time.Millisecond)
-	for _, prod := range shop.Products {
-		if prod.Name != product {
-			continue
-		}
-
-		if prod.Price > user.GetBalance() {
-			return errors.New("[Магазин] Недостаточно средств для покупки товара")
-		}
-
-		fmt.Printf("[Магазин] Товар [%s] - куплен!\n", prod.Name)
-	}
-
-	return nil
+// Facade - фасад
+type Facade struct {
+	subsystemA *SubsystemA
+	subsystemB *SubsystemB
+	subsystemC *SubsystemC
 }
 
-func (card Card) CheckBalance() error {
-	println("[Карта] Запрос в банк для проверки остатка")
-	time.Sleep(800 * time.Millisecond)
-
-	return card.Bank.CheckBalance(card.Name)
+func NewFacade() *Facade {
+	return &Facade{
+		subsystemA: &SubsystemA{},
+		subsystemB: &SubsystemB{},
+		subsystemC: &SubsystemC{},
+	}
 }
 
-func (bank Bank) CheckBalance(cardNumber string) error {
-	println(fmt.Sprintf("[Банк] Получение остатка по карте %s", cardNumber))
-	time.Sleep(300 * time.Millisecond)
-
-	for _, card := range bank.Cards {
-		if card.Name != cardNumber {
-			continue
-		}
-		if card.Balance <= 0 {
-			return errors.New("[Банк] Недостаточно средств")
-		}
-	}
-
-	println("[Банк] Остаток положительный!")
-
-	return nil
+// FacadeOperation - высокоуровневая операция, предоставляемая фасадом
+func (f *Facade) FacadeOperation() {
+	fmt.Println("Facade Operation:")
+	f.subsystemA.OperationA()
+	f.subsystemB.OperationB()
+	f.subsystemC.OperationC()
 }
-
-var (
-	bank = Bank{
-		Name:  "БАНК",
-		Cards: []Card{},
-	}
-
-	card1 = Card{
-		Name:    "CRD-1",
-		Balance: 200,
-		Bank:    &bank,
-	}
-
-	card2 = Card{
-		Name:    "CRD-2",
-		Balance: 5,
-		Bank:    &bank,
-	}
-
-	user1 = User{
-		Name: "Покупатель-1",
-		Card: &card1,
-	}
-
-	user2 = User{
-		Name: "Покупатель-2",
-		Card: &card2,
-	}
-
-	prod = Product{
-		Name:  "Сыр",
-		Price: 150,
-	}
-
-	shop = Shop{
-		Name: "SHOP",
-		Products: []Product{
-			prod,
-		},
-	}
-)
 
 func main() {
-	println("[Банк] Выпуск карт")
-	bank.Cards = append(bank.Cards, card1, card2)
-
-	fmt.Printf("[%s]\n", user1.Name)
-
-	err := shop.Sell(user1, prod.Name)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	fmt.Printf("[%s]\n", user2.Name)
-	err = shop.Sell(user2, prod.Name)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	// Использование фасада для упрощения взаимодействия с подсистемой
+	facade := NewFacade()
+	facade.FacadeOperation()
 }
